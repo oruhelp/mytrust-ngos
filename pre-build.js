1999468ed50709ce33841dd4bea1619ec9a3d572
karthikeyan-live <karthikeyan.live@hotmail.com>
2020-08-14 20:38:38 +0530

    copy java files

--- pre-build.js
+++ pre-build.js
@@ -2,10 +2,39 @@
 
 const fs = require('fs');
 const {exec} = require('child_process');
-let appName = 'test';
+let appName = '';
 exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
   if (err) {
     return;
+  }
+  function copyFile(_src, _dest) {
+    fs.copyFile(_src, _dest, _err => {
+      if (_err) {
+        console.log(
+          `PRE_BUILD: Error copying file from "${_src}" to "${_dest}"`,
+        );
+        console.log('Error', _err);
+        return;
+      }
+
+      console.log(`PRE_BUILD: Copyied file from "${_src}" to "${_dest}"`);
+    });
+  }
+  function replaceStringFromFile(_filePath, _srcString, _destString) {
+    fs.readFile(_filePath, 'utf8', function(_err, data) {
+      if (_err) {
+        return console.log(_err);
+      }
+
+      var regex = new RegExp(_srcString, 'g');
+      var result = data.replace(regex, _destString);
+
+      fs.writeFile(_filePath, result, 'utf8', function(__err) {
+        if (__err) {
+          return console.log(__err);
+        }
+      });
+    });
   }
   if (typeof stdout === 'string') {
     if (stdout.trim() === 'master') {
@@ -39,24 +68,31 @@ exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
     let data = JSON.stringify(buildAppConfig, null, '\t');
     fs.writeFileSync('./app.json', data);
 
-    // copy files
+    // copy firebase keys and theme
     copyFile(
       `./data/${appName}/google-services.json`,
       './android/app/google-services.json',
     );
     copyFile(`./data/${appName}/theme.js`, './constants/theme.js');
-  }
-  function copyFile(_src, _dest) {
-    fs.copyFile(_src, _dest, _err => {
-      if (_err) {
-        console.log(
-          `PRE_BUILD: Error copying file from "${_src}" to "${_dest}"`,
-        );
-        console.log('Error', _err);
-        return;
-      }
 
-      console.log(`PRE_BUILD: Copyied file from "${_src}" to "${_dest}"`);
-    });
+    // adding JAVA files
+    copyFile(
+      './data/common/MainActivity.java',
+      `./android/app/src/main/java/com/oruhelp/ngos/${appName}/MainActivity.java`,
+    );
+    copyFile(
+      './data/common/MainApplication.java',
+      `./android/app/src/main/java/com/oruhelp/ngos/${appName}/MainApplication.java`,
+    );
+    replaceStringFromFile(
+      `./android/app/src/main/java/com/oruhelp/ngos/${appName}/MainActivity.java`,
+      'mytrust1',
+      appName,
+    );
+    replaceStringFromFile(
+      `./android/app/src/main/java/com/oruhelp/ngos/${appName}/MainApplication.java`,
+      'mytrust1',
+      appName,
+    );
   }
 });
